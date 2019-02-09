d52e26ae7c1dd0280a8fc2f3875d3e5babcc2c0f
nongtiny <tinyutb3612@gmail.com>
2019-02-09 16:25:17 +0700

    [Fix] fix bug

--- Game.js
+++ Game.js
@@ -9,30 +9,27 @@ var simpleLevelPlan = `
 ......##############..
 ......................`;
 
-var Level = class Level{
-     constructor(plan){
-          let rows = plan.trim().split("\n").map( l => [...l]);
-          this.width = rows[0].length;
-          this.height = rows.length;
-          this.startActors = []
+var Level = class Level {
+     constructor(plan) {
+     let rows = plan.trim().split("\n").map(l => [...l]);
+     this.height = rows.length;
+     this.width = rows[0].length;
+     this.startActors = [];
 
-          this.rows = rows.map((row, y) => {
-               return row.map((ch, x) => {
-                    let type = levelChars[ch];
-                    if( typeof type == "string" ){
-                         return type;
-                    }
-                    this.startActors.push(
-                         type.create(new Vec(x, y), ch)
-                    );
-                    return "empty";
-               });
+     this.rows = rows.map((row, y) => {
+          return row.map((ch, x) => {
+          let type = levelChars[ch];
+          if (typeof type == "string") return type;
+          this.startActors.push(
+               type.create(new Vec(x, y), ch));
+          return "empty";
           });
-     }
+     });
+  }
 }
 
-var State = class State{
-     constructor(level, actors, status){
+var State = class State {
+     constructor(level, actors, status) {
           this.level = level;
           this.actors = actors;
           this.status = status;
@@ -42,87 +39,78 @@ var State = class State{
           return new State(level, level.startActors, "playing");
      }
 
-     get player(){
-          return this.actors.find( a => a.type == "player");
+     get player() {
+          return this.actors.find(a => a.type == "player");
      }
 }
 
-var Vec = class Vec{
-     constructor(x ,y){
-          this.x = x;
-          this.y = y;
+var Vec = class Vec {
+     constructor(x, y) {
+          this.x = x; this.y = y;
      }
-
-     plus(other){
+     plus(other) {
           return new Vec(this.x + other.x, this.y + other.y);
      }
-
-     times(factor){
+     times(factor) {
           return new Vec(this.x * factor, this.y * factor);
      }
 }
 
-var Player = class Player{
-     constructor(pos, speed){
+var Player = class Player {
+     constructor(pos, speed) {
           this.pos = pos;
           this.speed = speed;
      }
 
-     get type(){
-          return "player";
-     }
+     get type() { return "player"; }
 
-     static create(pos){
-          return new Player(pos.plus(new Vec(0, -0.5)), new Vec(0,0));
+     static create(pos) {
+          return new Player(pos.plus(new Vec(0, -0.5)),
+                         new Vec(0, 0));
      }
 }
 
-Player.prototype.size = new Vec(0.8,1.5);
+Player.prototype.size = new Vec(0.8, 1.5);
 
-var Lava = class Lava{
-     constructor(pos, speed, reset){
+var Lava = class Lava {
+     constructor(pos, speed, reset) {
           this.pos = pos;
           this.speed = speed;
           this.reset = reset;
      }
 
-     get type() {
-          return "lava";
-     }
+     get type() { return "lava"; }
 
-     static create(pos, ch){
-          if (ch == "="){
-               return new Lava(pos, new Vec(2,0));
-          }
-          else if (ch == "|"){
-               return new Lava(pos, new Vec(0,2));
-          }
-          else if (ch == "v"){
-               return new Lava(pos, new Vec(0,3), pos);
+     static create(pos, ch) {
+          if (ch == "=") {
+               return new Lava(pos, new Vec(2, 0));
+          } else if (ch == "|") {
+               return new Lava(pos, new Vec(0, 2));
+          } else if (ch == "v") {
+               return new Lava(pos, new Vec(0, 3), pos);
           }
      }
 }
 
-Lava.prototype.size = new Vec(1,1);
+Lava.prototype.size = new Vec(1, 1);
 
-var Coin = class Coin{
-     constructor(pos, basePos, wobble){
+var Coin = class Coin {
+     constructor(pos, basePos, wobble) {
           this.pos = pos;
           this.basePos = basePos;
           this.wobble = wobble;
      }
 
-     get type(){
-          return "coin";
-     }
+     get type() { return "coin"; }
 
-     static create(pos){
-          let basePos = pos.plus(new Vec(0.2,0.1));
-          return new Coin(basePos, basePos, Math.random() * Math.PI * 2);
+     static create(pos) {
+          let basePos = pos.plus(new Vec(0.2, 0.1));
+          return new Coin(basePos, basePos,
+                              Math.random() * Math.PI * 2);
      }
 }
 
-Coin.prototype.size = new Vec(0,6,0,6);
+Coin.prototype.size = new Vec(0.6, 0.6);
 
 var levelChars = {
      ".": "empty", 
@@ -131,47 +119,46 @@ var levelChars = {
      "@": Player, 
      "o": Coin,
      "=": Lava,
-      "|": Lava,
-       "v": Lava
+     "|": Lava, 
+     "v": Lava
 };
 
 var simpleLevel = new Level(simpleLevelPlan);
 
-function elt(name, attrs, ...children){
+function elt(name, attrs, ...children) {
      let dom = document.createElement(name);
-     for( let attr of Object.keys(attrs) ){
+     for (let attr of Object.keys(attrs)) {
           dom.setAttribute(attr, attrs[attr]);
      }
-     for( let child of children ){
-          dom.append(child);
+     for (let child of children) {
+          dom.appendChild(child);
      }
      return dom;
 }
 
-var DOMDisplay = class DOMDisplay{
-     constructor(parent, level){
+var DOMDisplay = class DOMDisplay {
+     constructor(parent, level) {
           this.dom = elt("div", {class: "game"}, drawGrid(level));
           this.actorLayer = null;
           parent.appendChild(this.dom);
      }
 
-     clear() { this.dom.remove() }
+     clear() { this.dom.remove(); }
 }
 
-const scale = 20;
+var scale = 20;
 
-function drawGrid(level){
+function drawGrid(level) {
      return elt("table", {
           class: "background",
-          stlye: `width: ${level.width * scale}px`
-          }, ...level.rows.map(row => 
-               elt("tr", {style: `height: ${scale}px`},
+          style: `width: ${level.width * scale}px`
+     }, ...level.rows.map(row =>
+          elt("tr", {style: `height: ${scale}px`},
                ...row.map(type => elt("td", {class: type})))
-          ));
+     ));
 }
 
-
-function drawActors(actors){
+function drawActors(actors) {
      return elt("div", {}, ...actors.map(actor => {
           let rect = elt("div", {class: `actor ${actor.type}`});
           rect.style.width = `${actor.size.x * scale}px`;
@@ -188,28 +175,29 @@ DOMDisplay.prototype.syncState = function(state) {
      this.dom.appendChild(this.actorLayer);
      this.dom.className = `game ${state.status}`;
      this.scrollPlayerIntoView(state);
-   };
+};
 
 DOMDisplay.prototype.scrollPlayerIntoView = function(state) {
      let width = this.dom.clientWidth;
      let height = this.dom.clientHeight;
      let margin = width / 3;
 
+     // The viewport
      let left = this.dom.scrollLeft, right = left + width;
      let top = this.dom.scrollTop, bottom = top + height;
 
      let player = state.player;
-     let center = player.pos.plus(player.size.times(0.5)).times(scale);
+     let center = player.pos.plus(player.size.times(0.5))
+                              .times(scale);
 
-     if (center.x < left + margin){
+     if (center.x < left + margin) {
           this.dom.scrollLeft = center.x - margin;
-     } else if (center.x > right - margin){
+     } else if (center.x > right - margin) {
           this.dom.scrollLeft = center.x + margin - width;
-     } 
-     if (center.y < top + margin){
+     }
+     if (center.y < top + margin) {
           this.dom.scrollTop = center.y - margin;
-     } else if (center.y > bottom - margin){
+     } else if (center.y > bottom - margin) {
           this.dom.scrollTop = center.y + margin - height;
      }
-}
-
+};
